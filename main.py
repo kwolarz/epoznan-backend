509b843273b068cac673262d8c5fc8066d0408d7
Krzysztof Wolarz <krzysztofwolarz@gmail.com>
2020-09-27 18:15:55 +0200

    add movie data

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from articleList import getArticleList
 from article import getArticle
 from home import getHomeData
 from comments import getComments
+from movie import getMovie
 
 
 data = {}
@@ -37,6 +38,10 @@ def comments(id, page):
     getComments(data, id, page)
     return json.dumps(data)
 
+@app.route('/movie/<url>')
+def movie(url):
+    getMovie(data, url)
+    return json.dumps(data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80) 
+    app.run(host='0.0.0.0') 
---
+++ movie.py
@@ -0,0 +1,77 @@
+import requests
+from bs4 import BeautifulSoup
+
+def getMovie(data, url):
+    data.clear()
+    url = 'https://epoznan.pl/' + url
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    title = soup.find(class_='post__title').text.replace('\n', '').replace('  ', '')
+    imageUrl = soup.find(class_='gallery__imageContainer')['href']
+
+    contentDiv = soup.find(class_='post__content')
+    text = contentDiv.find_all('p')[0].text.replace('\n', '').replace('  ', '')
+    cast = contentDiv.find_all('p')[1].text.replace('\n', '').replace('  ', '')
+
+    dates = []
+    datesHTML = soup.find_all('p')[2:7]
+    for index, date in enumerate(datesHTML):
+        # print(date.text.replace('\n', '').replace('  ', ''))
+        date = date.text.replace('\n', '').replace('  ', '')
+        dates.append({
+            str(index): date,
+        })
+        # dates.append(date)
+
+    repertoire = {}
+    post = soup.find(class_='singlePost__main')
+    for i, header in enumerate(post.find_all('p')[2:7], 1):
+        next_tag = header
+        j = 1
+
+        places = []
+        hourss = []
+
+        while True:
+            next_tag = next_tag.next_sibling
+            if next_tag is None or next_tag.name == 'p' or next_tag.name == 'div':
+                break
+            if next_tag.name is not None:
+                # print('{} - {}/{} - {}'.format(i, header.text, j, next_tag.string))
+                # print(next_tag.text)
+
+                place = next_tag.find(class_='eventsList__itemInfo icon-location').text
+                hours = next_tag.find(class_='eventsList__itemInfo icon-time').text.replace('.', ':').split(', ')
+                # print(places)
+
+                places.append(place)
+                hourss.append(hours)
+                repertoire[str(i)] = []
+
+
+                j += 1
+        
+        for index in range(len(places)): 
+            repertoire[str(i)].append({
+                'place': places[index],
+                'hours': hourss[index],
+            })
+
+    print(places)
+    print(hourss)
+    # print(title)
+    # print(imageUrl)
+    # print(text)
+    # print(cast)
+    # print(dates)
+
+    data['title'] = title
+    data['description'] = text
+    data['cast'] = cast
+    data['poster'] = imageUrl
+    data['dates'] = dates
+    data['repertoire'] = repertoire
+
+getMovie({}, 'culture-film-29558-mulan')
+#getMovie({}, 'culture-film-29547-25_lat_niewinnosci_sprawa_tomka_komendy')
